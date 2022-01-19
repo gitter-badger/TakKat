4027fd3dfdfea380efebcddcce39bc7ede3aa2a5
Mastcharub <91565083+Mastcharub@users.noreply.github.com>
2022-01-19 22:02:42 +0100

    commit 2

--- takkat/src/App.svelte
+++ takkat/src/App.svelte
@@ -1,17 +1,43 @@
 <script>
-    import { defaultEvmStores } from 'svelte-web3'
-    import { connected, web3, selectedAccount, chainId, chainData } from 'svelte-web3'
-    defaultEvmStores.setProvider()
+	import { defaultEvmStores, selectedAccount } from 'svelte-web3';
+	export let password = document.getElementById('password');
+	defaultEvmStores.setProvider();
 </script>
 
 <main>
-    <div id="title">
-        <h1><b>Welcome to the Decentralized Social Media!</b></h1>
-    </div>
+	<div id="header">
+		<b><h1>Welcome to the decentralized Social Media!</h1></b>
+	</div><br>
+	<div id="sign">
+  		<label for="username">Username:</label>
+		<input type="text" id="username" name="username" ><br><br>
+		<label for="password">Password:</label>
+		<input type="password" id="password" name="password" ><br><br>
+		<button onclick="signup()">Sign Up</button><br>
+	</div>
+	
 </main>
 
 <style>
-    #title{
-        text-align: center;
-    }
+	#header{
+		border: 10px solid black;
+  		margin-top: 100px;
+  		margin-bottom: 100px;
+  		margin-right: 150px;
+  		margin-left: 80px;
+		text-align: center;
+	}
+	#sign{
+		text-align: center;	
+	}
+	#sign > label{
+		text-align: center;	
+		cursor: pointer;
+		font-size: 2rem;	
+	}
+	#sign > button{
+		text-align: center;	
+		cursor: pointer;
+		font-size: 2rem;
+	}
 </style>
